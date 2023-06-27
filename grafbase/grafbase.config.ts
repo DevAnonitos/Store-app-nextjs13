64246cfa1a311bc6ecf617de538cf1d41c20f169
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-27 10:21:47 +0700

    update-Add-GrafbaseConfig

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,30 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+const post = g.model('Post', {
+  title: g.string(),
+  slug: g.string().unique(),
+  content: g.string().optional(),
+  publishedAt: g.datetime().optional(),
+  comments: g.relation(() => comment).optional().list().optional(),
+  likes: g.int().default(0),
+  tags: g.string().optional().list().length({ max: 5 }),
+  author: g.relation(() => user).optional()
+}).search()
+
+const comment = g.model('Comment', {
+  post: g.relation(post),
+  body: g.string(),
+  likes: g.int().default(0),
+  author: g.relation(() => user).optional()
+})
+
+const user = g.model('User', {
+  name: g.string(),
+  email: g.email().optional(),
+  posts: g.relation(post).optional().list(),
+  comments: g.relation(comment).optional().list()
+})
+
+export default config({
+  schema: g
+})
