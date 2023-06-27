79c8c8ca2d061a01bbdd520fee32f87d30add000
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-27 09:46:55 +0700

    update-Add-AuthProviders

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import { getProviders, signIn } from 'next-auth/react';
+import Button from './Button';
+
+type Provider = {
+    id: string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
+
 
 const AuthProviders = () => {
-    return (
-        <div>AuthProviders</div>
-    );
+
+    const [providers, setProviders] = useState<Providers | null>(null);
+
+    useEffect(() => {
+
+        const fetchProviders = async () => {
+            const res = await getProviders();
+
+            setProviders(res);
+        };
+
+        fetchProviders();
+
+    }, []);
+
+    if(providers) {
+        return (
+            <>
+                <div>
+                    {Object.values(providers).map((provider: Provider, i) => (
+                        <Button />
+                    ))}
+                </div>
+            </>
+        );
+    };
 };
 
 export default AuthProviders;
