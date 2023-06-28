751526746a1313d3c151b5454bd81ae4612595d6
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-28 10:24:02 +0700

    update-Add-FixKey

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
+import Image from 'next/image';
 
-const Button = () => {
+type Props = {
+    title: string;
+    leftIcon?: string | null,
+    rightIcon?: string | null,
+    handleClick?: MouseEventHandler,
+    submitting?: boolean | false,
+    type?: 'button' | 'submit',
+    bgColor?: string,
+    textColor?: string,
+};
+
+const Button = ({
+    title,
+    leftIcon,
+    rightIcon,
+    handleClick,
+    submitting,
+    type,
+    bgColor,
+    textColor,
+}: Props) => {
     return (
         <div>Button</div>
     );
