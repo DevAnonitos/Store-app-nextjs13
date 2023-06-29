4395c4ad5fdedad7d5468a91fdb627ef64353583
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-29 14:56:37 +0700

    update-Add-Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -23,7 +23,38 @@ const Button = ({
     textColor,
 }: Props) => {
     return (
-        <div>Button</div>
+        <>
+            <button
+                type={type || 'button'}
+                disabled={submitting || false}
+                className={`flexCenter gap-3 px-4 py-3
+                ${textColor ? textColor : 'text-white'}
+                ${submitting
+                    ? 'bg-black/50'
+                    : bgColor
+                    ? bgColor
+                    : 'bg-primary-purple'
+                } rounded-xl text-sm font-medium max-md:w-full`}
+            >
+                {leftIcon
+                    && <Image
+                            src={leftIcon}
+                            width={14}
+                            height={14}
+                            alt='LeftIcon'
+                        />
+                }
+                {title}
+                {rightIcon
+                    && <Image
+                            src={rightIcon}
+                            width={14}
+                            height={14}
+                            alt='LeftIcon'
+                        />
+                }
+            </button>
+        </>
     );
 };
 
