d1a0986d62d66ac4448f440f31d469550122fd40
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-29 23:40:13 +0700

    update-add-FormField

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,8 +1,51 @@
+type Props = {
+    type?: string;
+    title: string;
+    state: string;
+    placeholder: string;
+    isTextArea?: boolean;
+    setState: (value: string) => void;
+}
+
 import React from 'react';
 
-const FormField = () => {
+const FormField = ({
+    type,
+    title,
+    state,
+    placeholder,
+    isTextArea,
+    setState,
+}: Props) => {
     return (
-        <div>FormField</div>
+        <>
+            <div className='flexStart flex-col w-full gap-4'>
+                <label className='w-full text-gray-100'>
+                    {title}
+                </label>
+                {isTextArea ? (
+                    <>
+                        <textarea
+                            placeholder={placeholder}
+                            value={state}
+                            className='form_field-input'
+                            onChange={(e) => setState(e.target.value)}
+                        />
+                    </>
+                ): (
+                    <>
+                        <input
+                            type={type || 'text'}
+                            placeholder={placeholder}
+                            required
+                            value={state}
+                            className='form_field-input'
+                            onChange={(e) => setState(e.target.value)}
+                        />
+                    </>
+                )}
+            </div>
+        </>
     );
 };
 
