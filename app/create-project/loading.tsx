ad65d439b002a735935cff49ac4f5c6f119e0e5c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-30 16:49:16 +0700

    update-Add-LoadingComponents

--- app/create-project/loading.tsx
+++ app/create-project/loading.tsx
@@ -2,11 +2,42 @@
 
 import React from 'react';
 
+import { motion } from 'framer-motion';
+
+const loaderVariants = {
+    loading: { scale: 1.2, opacity: 1 },
+    hidden: { scale: 0.8, opacity: 0 },
+};
+
 const Loading = () => {
     return (
-        <div className='flex items-center justify-center'>
-
-        </div>
+        <>
+            <div className='flex items-center justify-center'>
+                <motion.svg
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    variants={loaderVariants}
+                    animate="loading"
+                    transition={{
+                        type: 'spring',
+                        stiffness: 260,
+                        damping: 20,
+                        yoyo: Infinity,
+                    }}
+                >
+                    <circle
+                        cx="25"
+                        cy="25"
+                        r="20"
+                        fill="none"
+                        strokeWidth="5"
+                        stroke="#9747FF"
+                        strokeLinecap="round"
+                    /> 
+                </motion.svg>
+            </div>
+        </>
     );
 };
 
