c39e368428e6d6540d5f358c5d636c6699e7b8ed
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-01 23:50:19 +0700

    update-AddLoadingMore

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
+"use client";
 
-const LoadMore = () => {
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import Button from './Button';
+
+type Props = {
+    startCursor: string;
+    endCursor: string;
+    hasPreviousPage: boolean;
+    hasNextPage: boolean;
+};
+
+const LoadMore = ({
+    startCursor,
+    endCursor,
+    hasPreviousPage,
+    hasNextPage,
+}: Props) => {
+
+    const router = useRouter();
+
+    const handleNavigation = (type: string) => {
+
+    };
+
     return (
-        <div>LoadMore</div>
+        <>
+            <div className='w-full flexCenter gap-5 mt-10'>
+                {hasPreviousPage && (
+                    <Button
+                        title='First Page'
+                        handleClick={() => handleNavigation('prev')}
+                    />
+                )}
+                {hasNextPage && (
+                    <Button
+                        title='Next shots'
+                        handleClick={() => handleNavigation('next')}
+                    />
+                )}
+                LoadingMore
+            </div>
+        </>
     );
 };
 
