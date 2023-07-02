635bd31bc82e898f334cd6c9b6ef9e426a23584b
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-02 23:42:22 +0700

    update-Add_hooK

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+"use client";
 
-export default function Modal () {
+import React, { useCallback, useRef, ReactNode } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+export default function Modal ({ children }: {
+    children: ReactNode
+}) {
+
+    const overplay = useRef<HTMLDivElement>(null);
+    const wrapper = useRef<HTMLDivElement>(null);
+    const router = useRouter();
+
     return (
         <>
             Modal
         </>
-    )
+    );
 };
