d5ab320dc06733301dcb823bac3c9cc83cf49548
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-03 20:51:50 +0700

    update-Add-CateGories

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,8 +1,42 @@
+"use client";
+
 import React from 'react';
+import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation';
+import { categoryFilters } from '@/constant';
 
 const Categories = () => {
+
+    const router = useRouter();
+    const pathName = usePathname();
+    const searchParams = useSearchParams();
+
+    const category = searchParams.get("category");
+
+    const handleTags = (item: string) => {
+        router.push(`${pathName}?category=${item}`);
+    };
+
     return (
-        <div>Categories</div>
+        <>
+            <div className='flexBetween w-full gap-5 flex-wrap'>
+                <div className='flex gap-2 overflow-auto'>
+                    {categoryFilters.map((filter) => (
+                        <button
+                            key={filter}
+                            type='button'
+                            onClick={() => handleTags(filter)}
+                            className={`${
+                                category === filter
+                                ? "bg-light-white-300 font-medium"
+                                : "font-normal"
+                            } px-4 py-3 rounded-lg capitalize whitespace-nowrap`}
+                        >
+                            {filter}
+                        </button>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 };
 
