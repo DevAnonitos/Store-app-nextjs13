d621f72e5421a7ea0d9bbbf5aa84712d69100204
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-04 23:29:30 +0700

    update-AddStyleProfileMenu

--- components/ProfileMenu.tsx
+++ components/ProfileMenu.tsx
@@ -1,8 +1,59 @@
-import React from 'react';
+"use client";
 
-const ProfileMenu = () => {
+import React, { Fragment, use, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { signOut } from 'next-auth/react';
+import { Menu, Transition } from '@headlessui/react';
+
+import { SessionInterface } from '@/common.types';
+
+const ProfileMenu = (
+    { session }: { session: SessionInterface }
+) => {
+
+    const [openModal, setOpenModal] = useState(false);
+
     return (
-        <div>ProfileMenu</div>
+        <>
+            <div className='flexCenter z-10 flex-col relative'>
+                <Menu as="div">
+                    <Menu.Button
+                        className="flexCenter"
+                        onMouseEnter={() => setOpenModal(true)}
+                    >
+                        {session?.user?.image && (
+                            <Image
+                                src={session.user.image}
+                                width={40}
+                                height={40}
+                                className='rounded-full object-contain ring-2'
+                                alt='UserProfileImage'
+                            />
+                        )}
+                    </Menu.Button>
+
+                    <Transition
+                        show={openModal}
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items
+                            static
+                            className="flexStart profile_menu-items"
+                            onMouseLeave={() => setOpenModal(false)}
+                        >
+                            
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
+            </div>
+        </>
     );
 };
 
