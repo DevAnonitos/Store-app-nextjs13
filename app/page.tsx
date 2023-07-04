d38bd7b323af0c143afc3cfd9af5cf0737b744fe
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-04 21:51:42 +0700

    update-Add-PropsHomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,44 @@
 
 import React from 'react';
+import { ProjectInterface } from '@/common.types';
 import { LoadMore, Categories, ProjectCard } from '@/components';
 
-const Home = async () => {
+type SearchParams = {
+  category?: string | null;
+  endcursor?: string | null;
+};
+
+type Props = {
+  searchParams: SearchParams,
+};
+
+type ProjectSearch = {
+  projectSearch: {
+    edges: {
+      node: ProjectInterface
+    }[];
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+      startCursor: string;
+      endCursor: string;
+    };
+  },
+};
+
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+export const revalidate = 0;
+
+const Home = async ({
+  searchParams: {
+    category,
+    endcursor,
+  },
+}: Props) => {
+
+
+
   return (
     <>
       <section className='flexStart flex-col paddings mb-16'>
