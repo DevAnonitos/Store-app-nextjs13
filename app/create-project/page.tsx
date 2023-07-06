8240597d7d6ae9edbee13c8dcc7dabf546886180
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-06 23:36:35 +0700

    update-Add-CreateProject-Page

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { redirect } from 'next/navigation';
+import { Modal, ProjectForm } from '@/components';
+
 
 const CreateProject = async () => {
+
+    const session = {};
+
+    if(!session) {
+        redirect("/");
+    }
+
     return (
-        <div>CreateProject</div>
+        <>
+            <Modal>
+                <h3 className='modal-head-text'>
+                    Create a New Project
+                </h3>
+
+                <ProjectForm />
+            </Modal>
+        </>
     );
 };
 
