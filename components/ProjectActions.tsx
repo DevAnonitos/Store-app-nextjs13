ad92d0d70ba0331d04c09b3f5eaf248116469ee8
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-08 23:13:41 +0700

    update-Add-ProjectActions

--- components/ProjectActions.tsx
+++ components/ProjectActions.tsx
@@ -1,8 +1,69 @@
-import React from 'react';
+"use client";
 
-const ProjectActions = () => {
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+
+type Props = {
+    projectId: string;
+};
+
+const ProjectActions = ({ projectId }: Props) => {
+
+    const [isDeleting, setIsDeleting] = useState<boolean>(false);
+    const router = useRouter();
+
+    const handleDeleteProject = async () => {
+        setIsDeleting(true);
+
+        try {
+
+            toast.success('🦄 Delete project is successfully!');
+            router.push("/");
+        } catch (error: any) {
+            console.log(error.message);
+        } finally {
+            setIsDeleting(false);
+        }
+    };
+
     return (
-        <div>ProjectActions</div>
+        <>
+            <Link
+                href={`/edit-project/${projectId}`}
+                className='flexCenter edit-action_btn'
+            >
+                <Image
+                    src="/pencile.svg"
+                    width={15}
+                    height={15}
+                    alt='EditImage'
+                />
+            </Link>
+
+            <button
+                type="button"
+                disabled={isDeleting}
+                className={`
+                    flexCenter
+                    delete-action_btn
+                    ${isDeleting
+                        ? "bg-gray"
+                        : "bg-primary-purple"
+                    }`
+                }
+                onClick={handleDeleteProject}
+            >
+                <Image
+                    src="/trash.svg"
+                    width={15}
+                    height={15}
+                    alt="delete"
+                />
+            </button>
+        </>
     );
 };
 
