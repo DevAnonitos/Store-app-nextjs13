2d125b80ab96c6b600b6f9f3efe0d0a6d2ee9297
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-09 10:16:48 +0700

    update-Add-RelatedProject

--- components/RelatedProjects.tsx
+++ components/RelatedProjects.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
 
-const RelatedProjects = () => {
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { ProjectInterface, UserProfile } from '@/common.types';
+
+type Props = {
+    userId: string;
+    projectId: string;
+}
+
+const RelatedProjects = ({ userId, projectId }: Props) => {
     return (
-        <div>RelatedProjects</div>
+        <>
+            <section className='flex flex-col mt-32 w-full'>
+                <div className='flexBetween'>
+                    <p className='text-base font-bold'>
+                        More by {" "}
+                    </p>
+                    <Link
+                        href={`/profile`}
+                        className="text-primary-purple text-base"
+                    >
+                        View All
+                    </Link>
+
+                    <div className='related_project-grid'>
+                        {}
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
 
